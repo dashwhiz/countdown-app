b1ebb2b46783ae04d567c198c6331dd4be0824a5
Aleksandar Velichkovikj <dashwhiz@gmail.com>
2025-10-30 11:28:50 +0100

    fix: Android configuration fixes
    - Fix package name mismatch (com.dashwhiz.countdown_app)
    - Add INTERNET permission to AndroidManifest
    - Add dependency overrides for flutter_web_auth_2 v4.1.0
    - Fixes app crash and Appwrite connectivity on Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.dashwhiz.countdown"
+    namespace = "com.dashwhiz.countdown_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.dashwhiz.countdown"
+        applicationId = "com.dashwhiz.countdown_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
